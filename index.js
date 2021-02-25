62e04ed346d8585e8a724851f0a212c5350e5b72
github username <github email address>
2021-02-25 10:21:49 -0800

    commit tree b8cffe1229d6cd8651784fc6a843343c580176fa

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = "Daniel";
+const upperCaseCurrentUser = currentUser.toUpperCase()
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`; 
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser}!`;
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
